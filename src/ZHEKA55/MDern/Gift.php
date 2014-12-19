e8dd1c20aa8d7385d0756c5366e64bff30c6d6c0
zheka55 <sev2002_2010@maill.ru>
2014-12-19 12:19:08 +0600

    Create Gift.php

---
+++ src/ZHEKA55/MDern/Gift.php
@@ -0,0 +1,19 @@
+<?php
+namespace ZHEKA55\MDern;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+class Gift extends PluginTask {
+  public function __construct($plugin) {
+    $this->plugin = $plugin;
+    $this->start = false;
+    parent::__construct($plugin);
+  }
+  public function onRun($ticks) {
+    if($this->start) {
+      $this->plugin->giveAll();
+    } else {
+      $this->start = true;
+    }
+  }
+}
+?>
